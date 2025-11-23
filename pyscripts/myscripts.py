0eda315d2adea314cc88b3fa6a61a8e1d2a3d350
nolscod3 <nolscod3@email.com>
2025-11-23 09:07:49 +0000

    created basic python scripts

---
+++ pyscripts/myscripts.py
@@ -0,0 +1,9 @@
+# A basic Python script
+
+def greet(name):
+    """Function to greet the user."""
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
